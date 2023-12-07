8c7cf24f86067e8de2a2ceddb4abfd156455b374
Kyle Close <k.james.close@gmail.com>
2023-12-06 16:18:49 -0800

    further progress on auth

--- src/components/Authentication.tsx
+++ src/components/Authentication.tsx
@@ -1,7 +1,32 @@
 import { Box } from '@mui/material';
+import authImg from '../assets/auth.png';
 
 function Authentication() {
-  return <Box sx={{ display: 'flex', flex: '1' }}></Box>;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: { xs: 'column' },
+        flex: '1',
+        maxHeight: '100%',
+      }}
+    >
+      <Box
+        component='img'
+        alt='Side profile of woman made up of various fruits and vegetables'
+        src={authImg}
+        sx={{
+          display: { xs: 'none', sm: 'block' },
+          objectFit: 'contain',
+          maxWidth: { sm: '50%', md: '33%' },
+          backgroundColor: '#c7c7c7',
+          height: '100%',
+        }}
+      ></Box>
+
+      <Box sx={{ flex: 1 }}></Box>
+    </Box>
+  );
 }
 
 export default Authentication;
