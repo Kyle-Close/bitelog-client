64b3cf46f001ec564ed1625e414ee0c0c12adde7
Kyle Close <k.james.close@gmail.com>
2023-12-10 13:00:42 -0800

    Revert "jest stuff"
    This reverts commit d11baf473973bd6c2989ec048ebb4e1c090efd2e.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
-import Authentication from './components/auth/Authentication';
+import Authentication from './components/Authentication';
 
 export const router = createBrowserRouter([
   {
