4abbc938bf1df8ef21ea7d0f27f7a7d1f3a97d65
Kyle Close <k.james.close@gmail.com>
2023-12-14 23:55:40 -0800

    user logged in state persists over page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Box, CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './theme';
 import ResponsiveAppBar from './components/header';
-import { ReactNode } from 'react';
+import { ReactNode, useContext, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './firebaseConfig';
-import { UserProvider } from './contexts';
+import { UserContext } from './contexts';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 interface AppProps {
   children?: ReactNode;
@@ -13,16 +14,37 @@ interface AppProps {
 export const app = initializeApp(firebaseConfig);
 
 function App({ children }: AppProps) {
+  const { setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    const auth = getAuth();
+
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { email, displayName } = user;
+
+        if (!email || !displayName) {
+          setUser(null);
+          return;
+        }
+
+        setUser({ email, username: displayName });
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <UserProvider>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-          <ResponsiveAppBar />
-          {children}
-        </Box>
-      </ThemeProvider>
-    </UserProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <ResponsiveAppBar />
+        {children}
+      </Box>
+    </ThemeProvider>
   );
 }
 
