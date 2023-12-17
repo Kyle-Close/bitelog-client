6b00983bbe76f1e283cc06ae92715f405f9c7ebd
Kyle-Close <k.james.close@gmail.com>
2023-12-17 15:04:22 -0800

    save

--- src/__test__/register.test.tsx
+++ src/__test__/register.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { UserContext } from '../contexts';
 import Register from '../components/register';
@@ -8,6 +8,12 @@ import { signInWithPopup } from 'firebase/auth';
 
 jest.mock('firebase/auth');
 jest.mock('../hooks/useRegisterForm');
+
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as any),
+  useNavigate: () => mockedUsedNavigate,
+}));
 
 beforeEach(() => {
   (useRegisterForm as jest.Mock).mockReturnValue({
@@ -31,6 +37,7 @@ const setup = (isLoggedIn: boolean) => {
     ? { username: 'test', email: 'test@gmail.com' }
     : null;
   const provider = { user, LoginUser: jest.fn(), ClearUserContext: jest.fn() };
+
   return render(
     <UserContext.Provider value={provider}>
       <BrowserRouter>
@@ -41,13 +48,30 @@ const setup = (isLoggedIn: boolean) => {
 };
 
 describe('<Register />', () => {
-  const res = { user: { accessToken: 'token' } };
-  (signInWithPopup as jest.Mock).mockResolvedValue(res);
-
   it('register with Google button in on the page and redirects upon success', async () => {
+    (signInWithPopup as jest.Mock).mockResolvedValue({
+      user: { email: 'test@gmail.com', displayName: 'test' },
+    });
+
     setup(false);
+
+    const googleBtn = screen.getByRole('button', {
+      name: /register with google/i,
+    });
+    await fireEvent.click(googleBtn);
+
     await waitFor(() => {
-      expect(screen.getByText(/register with google/i)).toBeInTheDocument();
+      expect(mockedUsedNavigate).toHaveBeenCalledWith('/');
+    });
+  });
+
+  it('username input is rendered and functional', async () => {
+    setup(false);
+    const usernameInput = screen.getByPlaceholderText(/username/i);
+    await fireEvent.change(usernameInput, { target: { value: 'update' } });
+
+    await waitFor(() => {
+      expect(screen.getByDisplayValue(/update/i)).toBeInTheDocument;
     });
   });
 });
