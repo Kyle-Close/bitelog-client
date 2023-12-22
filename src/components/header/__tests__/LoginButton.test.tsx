47d5f870f670e36ef8d74ea834ab0a235ec64da4
Kyle Close <k.james.close@gmail.com>
2023-12-21 22:12:58 -0800

    bruh

--- src/components/header/__tests__/LoginButton.test.tsx
+++ src/components/header/__tests__/LoginButton.test.tsx
@@ -1,11 +1,27 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '../../../test-utils';
-import LoggedInButtons from '../LoggedInButtons';
+import { getAuth } from 'firebase/auth';
+import LoggedOutButtons from '../LoggedOutButtons';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter, useNavigate } from 'react-router-dom';
+
+jest.mock('firebase/auth');
+jest.mock('react-router-dom', () => ({
+  __esModule: true,
+  ...jest.requireActual,
+  useNavigate: jest.fn(),
+}));
 
 describe('Header login button', () => {
+  (getAuth as jest.Mock).mockResolvedValueOnce(true);
+  (useNavigate as jest.Mock).mockImplementation(jest.fn());
+
   it('Button is rendered on the header when not logged in', () => {
-    render(<LoggedInButtons />);
+    render(
+      <BrowserRouter>
+        <LoggedOutButtons />
+      </BrowserRouter>
+    );
     const loginBtn = screen.getByText(/login/i);
-    expect(loginBtn);
+    expect(loginBtn).toBeInTheDocument();
   });
 });
