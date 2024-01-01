c888d8e9366d3067aebb27de81320734c1d89f75
Kyle Close <k.james.close@gmail.com>
2024-01-01 14:39:06 -0800

    save

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,9 +1,29 @@
 import { Box, Typography } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../contexts';
+import { getAuth } from 'firebase/auth';
 
 function LandingPage() {
+  const user = useContext(UserContext);
+  const [jwtToken, setJwtToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchJWT = async () => {
+      const authUser = getAuth().currentUser;
+      if (authUser) {
+        const token = await authUser.getIdToken();
+        setJwtToken(token);
+      }
+    };
+
+    if (user) {
+      fetchJWT();
+    }
+  }, [user]); // Dependency array ensures this effect runs when `user` changes
+
   return (
     <Box>
-      <Typography>Homepage</Typography>
+      <Typography>{`My JWT Token: ${jwtToken}`}</Typography>
     </Box>
   );
 }
