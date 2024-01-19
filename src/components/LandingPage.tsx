8b818d90920c35c15e647f15ccde25fe078e801c
Kyle Close <k.james.close@gmail.com>
2024-01-18 19:53:00 -0800

    save

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -2,8 +2,23 @@ import { Box, Typography } from '@mui/material';
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../contexts';
 import { getAuth } from 'firebase/auth';
+import { BASE_URL } from '../config/axiosConfig';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchIngredients = async () => {
+  const url = BASE_URL + '/ingredients';
+  const res = await fetch(url);
+  return res.json();
+};
 
 function LandingPage() {
+  const { isError, isLoading, data } = useQuery({
+    queryKey: ['ingredients'],
+    queryFn: fetchIngredients,
+  });
+
+  console.log(data.ingredients);
+
   const user = useContext(UserContext);
   const [jwtToken, setJwtToken] = useState<string | null>(null);
 
@@ -24,6 +39,13 @@ function LandingPage() {
   return (
     <Box>
       <Typography>{`My JWT Token: ${jwtToken}`}</Typography>
+      <Box sx={{ pt: '4rem', pl: '2rem', border: '2px solid red' }}>
+        {isError && 'Error fetching data'}
+        {isLoading && 'Loading...'}
+        {!isError &&
+          !isLoading &&
+          data.ingredients.map((ing: { id: Number; name: string }) => ing.name)}
+      </Box>
     </Box>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { UserProvider } from './contexts/UserContext.tsx';
 import { createRoot } from 'react-dom/client';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
+const client = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <UserProvider>
-      <RouterProvider router={router} />
-    </UserProvider>
+    <QueryClientProvider client={client}>
+      <UserProvider>
+        <RouterProvider router={router} />
+      </UserProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
