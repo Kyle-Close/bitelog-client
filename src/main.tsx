5398c881dd671fce9ebde1b43efd4fdae8173431
Kyle Close <k.james.close@gmail.com>
2024-03-14 11:25:17 -0700

    done refactor on folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
 import { UserProvider } from './contexts/UserContext.tsx';
 import { createRoot } from 'react-dom/client';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import Router from './routes/index.tsx';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
@@ -14,7 +14,9 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
       <UserProvider>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </UserProvider>
     </QueryClientProvider>
   </React.StrictMode>
---
+++ src/routes/PagesData.tsx
@@ -0,0 +1,90 @@
+import App from '../App';
+import LandingPage from '../pages/landing';
+import Journal from '../pages/journal';
+import PageSelector from '../pages/journal/PageSelector';
+import Scheduler from '../pages/scheduler';
+import Authentication from '../components/authentication';
+import FoodsPage from '../pages/food';
+import IngredientsPage from '../pages/ingredients';
+import SettingsForm from '../pages/journal/Settings';
+
+const pagesData = [
+  {
+    path: '',
+    element: (
+      <App>
+        <LandingPage />
+      </App>
+    ),
+    title: 'home',
+  },
+  {
+    path: '/user/:userId/journal/:journalId',
+    element: (
+      <App>
+        <Journal>
+          <PageSelector />
+        </Journal>
+      </App>
+    ),
+    title: 'journal',
+  },
+  {
+    path: '/user/:userId/journal/:journalId/scheduler',
+    element: (
+      <App>
+        <Scheduler view='hourly' />
+      </App>
+    ),
+    title: 'scheduler',
+  },
+  {
+    path: '/login',
+    element: (
+      <App>
+        <Authentication isLogin={true} />
+      </App>
+    ),
+    title: 'login',
+  },
+  {
+    path: '/register',
+    element: (
+      <App>
+        <Authentication isLogin={false} />
+      </App>
+    ),
+    title: 'register',
+  },
+  {
+    path: '/user/:userId/food',
+    element: (
+      <App>
+        <FoodsPage />
+      </App>
+    ),
+    title: 'foods',
+  },
+  {
+    path: '/user/:userId/ingredients',
+    element: (
+      <App>
+        <IngredientsPage />
+      </App>
+    ),
+    title: 'ingredients',
+  },
+  {
+    path: '/user/:userId/journal/:journalId/settings',
+    element: (
+      <App>
+        <Journal>
+          <SettingsForm />
+        </Journal>
+      </App>
+    ),
+    title: 'journal',
+  },
+];
+
+export default pagesData;
