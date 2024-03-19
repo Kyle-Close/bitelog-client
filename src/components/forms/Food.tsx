5649218dda6ec9ae31bd19c412275860fe792b76
kyle-close <k.james.close@gmail.com>
2024-03-18 21:43:18 -0700

    more progress on food form

--- src/components/forms/Food.tsx
+++ src/components/forms/Food.tsx
@@ -1,5 +1,6 @@
 import { Autocomplete, Box, Button, Divider, InputLabel, TextField } from '@mui/material';
 import { useFoodForm } from '../../hooks/useFoodForm';
+import { useEffect, useState } from 'react';
 
 interface IngredientType {
   name: string;
@@ -7,12 +8,29 @@ interface IngredientType {
 
 export function FoodForm() {
   const { ingredientsQuery } = useFoodForm();
+  const [value, setValue] = useState<IngredientType | null>(null);
+  const [inputValue, setInputValue] = useState('');
+  const [selectedIngredients, setSelectedIngredients] = useState<string[]>([]);
 
   if (!ingredientsQuery || !ingredientsQuery.data) return;
 
   const defaultProps = {
     options: ingredientsQuery.data,
     getOptionLabel: (option: IngredientType) => option.name,
+  };
+
+  const handleAutoCompleteChange = (event: any, newValue: IngredientType | null) => {
+    setValue(newValue);
+    if (newValue) addNewSelectedIngredient(newValue.name);
+  };
+
+  const handleInputChange = (event: React.SyntheticEvent<Element, Event>, newInputValue: string) => {
+    setInputValue(newInputValue);
+  };
+
+  const addNewSelectedIngredient = (ingredient: string) => {
+    if (selectedIngredients.includes(ingredient)) return;
+    setSelectedIngredients((prev) => [...prev, ingredient]);
   };
 
   return (
@@ -28,12 +46,17 @@ export function FoodForm() {
           renderInput={(params) => <TextField {...params} variant='standard' />}
           id='combo-ingredients'
           size='small'
+          value={value}
+          onChange={handleAutoCompleteChange}
+          inputValue={inputValue}
+          onInputChange={handleInputChange}
         ></Autocomplete>
       </Box>
       <Button sx={{ mt: '2rem' }} color='secondary' variant='contained' type='submit' size='small'>
         Submit
       </Button>
       <Divider />
+      {selectedIngredients.length > 0 && selectedIngredients.join(', ')}
     </Box>
   );
 }
