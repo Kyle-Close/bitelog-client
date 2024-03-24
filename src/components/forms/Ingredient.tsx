61a210606913459ccd7163af4be98b646a0d1bc8
kyle-close <k.james.close@gmail.com>
2024-03-24 10:53:53 -0700

    done create ingredient form

--- src/components/forms/Ingredient.tsx
+++ src/components/forms/Ingredient.tsx
@@ -1,13 +1,22 @@
-import { Box, Button, InputLabel, TextField } from '@mui/material';
+import { Box, Button, InputLabel, TextField, Typography } from '@mui/material';
 import { useIngredientForm } from '../../hooks/useIngredientForm';
+import { Loading } from '../generic/Loading';
 
 export function IngredientForm() {
-  const { ingredientName, handleIngredientNameChange } = useIngredientForm();
+  const {
+    ingredientName,
+    handleIngredientNameChange,
+    handleSubmit,
+    createIngredientMutation,
+  } = useIngredientForm();
+
+  if (createIngredientMutation.isPending) return <Loading />;
+
   return (
     <Box
       sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}
       component='form'
-      onSubmit={(e) => {}}
+      onSubmit={handleSubmit}
     >
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
         <InputLabel id='food-name'>Ingredient name</InputLabel>
@@ -19,6 +28,16 @@ export function IngredientForm() {
           id='ingredient-name'
         />
       </Box>
+      {createIngredientMutation.isSuccess && (
+        <Typography
+          color='lightgreen'
+          fontWeight={'bold'}
+          align='center'
+          fontSize={'small'}
+        >
+          Successfully created ingredient!
+        </Typography>
+      )}
       <Button
         sx={{ mt: '2rem' }}
         color='secondary'
--- src/hooks/useIngredientForm.ts
+++ src/hooks/useIngredientForm.ts
@@ -1,23 +1,54 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useContext, useState } from 'react';
 import { UserContext } from '../context';
+import { makeRequestToBackend } from '../helpers/utility';
+import { BASE_URL } from '../config/axiosConfig';
 
 export function useIngredientForm() {
   const { user } = useContext(UserContext);
   const [ingredientName, setIngredientName] = useState('');
+  const queryClient = useQueryClient();
+
   const createIngredientMutation = useMutation({
     mutationKey: ['ingredients', user?.uid],
+    mutationFn: () => submitForm(),
+    onSuccess: () => {
+      setIngredientName('');
+      if (user && user.uid) {
+        queryClient.invalidateQueries({ queryKey: ['ingredients', user.uid] });
+      }
+    },
   });
 
   const handleIngredientNameChange = (
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
     setIngredientName(e.target.value);
+    createIngredientMutation.reset();
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    createIngredientMutation.mutate();
   };
 
-  return { ingredientName, handleIngredientNameChange };
+  const submitForm = async () => {
+    try {
+      const result = await makeRequestToBackend({
+        url: `${BASE_URL}/user/${user?.uid}/ingredients`,
+        method: 'POST',
+        body: { name: ingredientName },
+      });
+      console.log(result);
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  return {
+    ingredientName,
+    handleIngredientNameChange,
+    handleSubmit,
+    createIngredientMutation,
+  };
 }
